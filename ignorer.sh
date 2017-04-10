95e77b4fac0cc23a03cdcb166b3cc9d47deed705
Henry <hps1945@live.co.uk>
2017-04-10 18:52:28 +0100

    Completed ignorer

--- ignorer.sh
+++ ignorer.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-while [ $# -gt 1 ]; do
-    echo $1
+touch .gitignore
+while [ $# -gt 0 ]; do
+    wget --spider -qO- https://raw.githubusercontent.com/Delphboy/gitignore/master/$1.gitignore
+    if [ $? -ne 0 ]; then
+        echo "Error downloading $1.gitignore"
+    else
+        echo "$(wget -qO- https://raw.githubusercontent.com/github/gitignore/master/$1.gitignore)" >> .gitignore
+    fi
     shift
 done
