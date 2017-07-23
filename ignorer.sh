a5769657dc4c2e94d726e5d07a03e067fde2553a
Delphboy <hps1945@live.co.uk>
2017-07-23 13:33:08 +0100

    Added --help and --list commands

--- ignorer.sh
+++ ignorer.sh
@@ -4,11 +4,14 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 touch .gitignore
 
-if [ $1 = "--help" ]; then
+if [ $1 = "--list" ]; then
     content=$(wget https://github.com/github/gitignore -q -O -) 
     #formated=$(echo $content) | grep -E "html"
     echo "These are the gitignore files currently available:"
     echo $content | grep -Eo ">[A-Za-z]+\.gitignore<" | sed 's/^.\(.*\).$/\1/'
+elif [ $1 = "--help" ]; then
+	echo "Usage: $ ignorer <ignorer file name> [<ignorer file name>]"
+	echo "$ ignorer --list will show all the gitignore files currently on GitHub's gitignore master branch"
 else
     while [ $# -gt 0 ]; do
         wget --spider -qO- https://raw.githubusercontent.com/Delphboy/gitignore/master/$1.gitignore
